731ffd96741a38b761ea342af926a056a4767aef
ParkYoungWoong <thesecon@gmail.com>
2025-07-17 10:02:03 +0900

    .DS_Store 제거.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,44 @@
 import { useState } from 'react'
+import Button from './components/Button.jsx'
 
 export default function App() {
-  return <></>
+  const [isLoadingForCancel, setIsLoadingForCancel] = useState(false)
+  const [isLoadingForSave, setIsLoadingForSave] = useState(false)
+  const [isLoadingForDelete, setIsLoadingForDelete] = useState(false)
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '18px',
+        alignItems: 'flex-start'
+      }}>
+      <Button
+        variant="secondary"
+        loading={isLoadingForCancel}
+        onClick={function () {
+          setIsLoadingForCancel(!isLoadingForCancel)
+        }}>
+        취소
+      </Button>
+      <Button
+        variant="primary"
+        width={100}
+        loading={isLoadingForSave}
+        onClick={function () {
+          setIsLoadingForSave(!isLoadingForSave)
+        }}>
+        저장
+      </Button>
+      <Button
+        variant="danger"
+        width={70}
+        loading={isLoadingForDelete}
+        onClick={function () {
+          setIsLoadingForDelete(!isLoadingForDelete)
+        }}>
+        삭제
+      </Button>
+    </div>
+  )
 }
