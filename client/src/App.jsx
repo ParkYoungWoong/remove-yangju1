4d8dad222273ea6bc19d67b5c51effa05ab6fcc2
ParkYoungWoong <thesecon@gmail.com>
2025-07-18 14:26:20 +0900

    프로젝트 모듈화 중..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,15 @@
-import { useEffect } from 'react'
-import TodoItem from './components/todos/TodoItem.jsx'
 import { useTodoStore } from './stores/todo.js'
 
 // 일반 함수 => function (매개변수) { return 데이터 }
 // 화살표 함수 => 매개변수 => 데이터
 
 export default function App() {
-  const todos = useTodoStore(s => s.todos)
-  const text = useTodoStore(s => s.text)
-  const setText = useTodoStore(s => s.setText)
-  const fetchTodos = useTodoStore(s => s.fetchTodos)
-  const createTodo = useTodoStore(s => s.createTodo)
   const ascending = useTodoStore(s => s.ascending)
   const descending = useTodoStore(s => s.descending)
   const setDone = useTodoStore(s => s.setDone)
 
-  useEffect(function () {
-    fetchTodos()
-    // eslint-disable-next-line
-  }, [])
-
   return (
     <>
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={function (event) {
-            setText(event.target.value)
-          }}
-          onKeyDown={function (event) {
-            if (event.nativeEvent.isComposing) return
-            if (event.key === 'Enter') createTodo()
-          }}
-        />
-        <button onClick={createTodo}>추가</button>
-      </div>
       <div>
         <button onClick={ascending}>오름차순</button>
         <button onClick={descending}>내림차순</button>
@@ -58,16 +32,6 @@ export default function App() {
           할 일
         </button>
       </div>
-      <ul>
-        {todos.map(function (todo) {
-          return (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-            />
-          )
-        })}
-      </ul>
     </>
   )
 }
--- test.js
+++ test.js
@@ -1,17 +1,11 @@
-const nums = [6, 2, 1, 8];
+// 원시형(Primitive)
+String;
+Number;
+Boolean;
+Null;
+Undefined;
 
-// 배열.filter()
-const newNums1 = nums.filter(function (num) {
-  return num < 3;
-});
-console.log(newNums1); // [2, 1]
-
-// for 반복문
-const newNums2 = [];
-for (let i = 0; i < nums.length; i += 1) {
-  const num = nums[i];
-  if (num < 3) {
-    newNums2.push(num);
-  }
-}
-console.log(newNums2); // [2, 1]
+// 참조형(Reference)
+Object;
+Array;
+Function;
